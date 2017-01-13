9b1e54da91b47c943ad71fc5924a53ea8804769e
unknown <zhuhuotui@gmail.com>
2017-01-13 10:55:48 +0800

    fix:ignore ssl error

--- util.py
+++ util.py
@@ -8,11 +8,35 @@ import requests.packages.urllib3 as urllib3
 urllib3.disable_warnings()
 
 
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+
+
+
+# Never check any hostnames
+class HostNameIgnoringAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       assert_hostname=False)
+
+
+# Check a custom hostname
+class CustomHostNameCheckingAdapter(HTTPAdapter):
+    def cert_verify(self, conn, url, verify, cert):
+        #      implement me
+        host = custom_function_mapping_url_to_hostname(url)
+        conn.assert_hostname = host
+        return super(CustomHostNameCheckingAdapter,
+                     self).cert_verify(conn, url, verify, cert)
+
 conn = None
 def open_url_and_keep_alive(url, proxies=None):
     global conn
     if conn == None:
         conn = requests.session()
+        conn.mount('https://', HostNameIgnoringAdapter())
     rep = conn.get(url, proxies=proxies)
     if 200 != rep.status_code:
         print rep
@@ -20,7 +44,9 @@ def open_url_and_keep_alive(url, proxies=None):
     return rep.content
 
 def open_url(url, proxies=None):
-    response  = requests.get(url, proxies=proxies)
+    conn = requests.session()
+    conn.mount('https://', HostNameIgnoringAdapter())
+    response  = conn.get(url, proxies=proxies)
     if 200 != response.status_code:
         print response.status_code
         return None
